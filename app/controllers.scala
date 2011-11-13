d1ea4777dd1fe1974492f5288136de88e03cc31d
mdodsworth <dodster.michael@googlemail.com>
2011-11-13 10:08:39 -0800

    adding the rest of the hello world play app

--- app/controllers.scala
+++ app/controllers.scala
@@ -7,8 +7,14 @@ object Application extends Controller {
     
     import views.Application._
     
-    def index = {
-        html.index("Your Scala application is ready!")
+    def index = html.index()
+    def sayHello = {
+        params.get("myName") match {
+            case name:String if name.trim().length() > 0 => html.sayHello(name)
+            case other => {
+                flash += ("error" -> "Oops, please enter your name!")
+                Action(index)
+            }
+        }
     }
-    
 }
